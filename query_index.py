112845cdd15c0fcf725a6f2ca13c046caad0bd04
yan.xin <yan.xin@darcytech.com>
2023-03-12 12:44:47 +0800

    去除代码中读取api_key

--- query_index.py
+++ query_index.py
@@ -6,8 +6,6 @@ from llama_index import GPTSimpleVectorIndex
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-# 设置api key
-api_key = os.getenv('OPENAI_API_KEY', default="<填入你的api key>")
 # 加载索引
 new_index = GPTSimpleVectorIndex.load_from_disk('index.json')
 # 查询索引
